efdc5b9741fe889cb245b0c3bc6633e70d6c7bc0
Sean Sullivan <dev@seanmedia.ca>
2020-03-30 22:11:21 +0000

    Implement assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
