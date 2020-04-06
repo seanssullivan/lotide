2523e292b148e3dd800fd6ee142f011aae682533
Sean Sullivan <dev@seanmedia.ca>
2020-04-06 20:19:15 +0000

    Refactor assertObjectsEqual function and add tests to assertObjectsEqualTest.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,35 +1,7 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// assertObjectsEqual.js
 
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
+const eqObjects = require('./eqObjects');
 
-  for (const key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// ACTUAL FUNCTION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -39,5 +11,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// TEST CASES
-assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1" });
+module.exports = assertObjectsEqual;
---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,6 @@
+// assertObjectsEqualTest.js
+
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+// TEST CASES
+assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1" });
