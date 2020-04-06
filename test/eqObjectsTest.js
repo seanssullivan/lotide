a5254d19bbdd1ead694dac153b03ecc4bc1bf141
Sean Sullivan <dev@seanmedia.ca>
2020-04-06 23:31:32 +0000

    Add tests to eqObjectsTest.js

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,25 +1,44 @@
 // eqObjectsTest.js
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
-// TEST CASES
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
+describe("#eqObjects", () => {
 
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
+  it("returns true for { a: '1', b: '2' } and { b: '2', a: '1' }", () => {
+    const ab = { a: "1", b: "2" };
+    const ba = { b: "2", a: "1" };
+    assert.equal(eqObjects(ab, ba), true);
+  });
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
+  it("returns false for { a: '1', b: '2' } and { a: '1', b: '2', c: '3' }", () => {
+    const ab = { a: "1", b: "2" };
+    const abc = { a: "1", b: "2", c: "3" };
+    assert.equal(eqObjects(ab, abc), false);
+  });
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
+  it("returns true for { c: '1', d: ['2', 3] } and { d: ['2', 3], c: '1' }", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const dc = { d: ["2", 3], c: "1" };
+    assert.equal(eqObjects(cd, dc), true);
+  });
 
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  it("returns false for { c: '1', d: ['2', 3] } and { c: '1', d: ['2', 3, 4] } ", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const cd2 = { c: "1", d: ["2", 3, 4] };
+    assert.equal(eqObjects(cd, cd2), false);
+  });
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  it("returns true for { a: { z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }", () => {
+    assert.equal(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  });
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+  it("returns false for { a: { y: 0, z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }", () => {
+    assert.equal(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  });
+
+  it("returns false for { a: { y: 0, z: 1 }, b: 2 } and { a: 1, b: 2 }", () => {
+    assert.equal(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+  });
+
+});
