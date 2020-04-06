56bdb4e50880d891b40f376cd36fb15c5a06df7e
Sean Sullivan <dev@seanmedia.ca>
2020-04-06 19:42:44 +0000

    Refactor middle tests and tail tests to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,30 @@
 // test/tailTest.js
 
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
 
-assertEqual(tail([1])[0], undefined);
-assertEqual(tail([])[0], undefined);
+  it("returns a new array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.lengthOf(words, 3);
+  });
+
+  it("returns an empty array for [1]", () => {
+    const output = tail([1]);
+    assert.isArray(output);
+    assert.isEmpty(output);
+  });
+
+  it("returns an empty array for []", () => {
+    const output = tail([]);
+    assert.isArray(output);
+    assert.isEmpty(output);
+  });
+
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.sameOrderedMembers(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+
+});
