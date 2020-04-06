746ef0a5fa09be94928a33f629577f26bf790358
Sean Sullivan <dev@seanmedia.ca>
2020-04-06 20:17:42 +0000

    Add index.js and require all utility functions

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const tail = require('./tail');
+const without = require('./without');
+
+module.exports = {
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  takeUntil: takeUntil,
+  tail: tail,
+  without: without
+}
